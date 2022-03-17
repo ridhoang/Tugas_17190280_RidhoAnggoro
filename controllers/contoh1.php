e3666c92db72a5f4068831fd854717990bb6f54e
ridhoang <101780347+ridhoang@users.noreply.github.com>
2022-03-17 17:10:02 +0700

    Add files via upload

---
+++ controllers/contoh1.php
@@ -0,0 +1,15 @@
+<?php
+class contoh1 extends CI_Controller
+{
+
+    public function index()
+
+        {
+            echo "<h1>perkenalan</h1>";
+            echo "Nama Saya Ridho Anggoro Yudi
+                  saya tinggal di daerah Rajeg
+                  Kab.Tangerang, olahraga yang 
+                  yang saya sukai adalah bermain 
+                  bola dan sepedahan";
+        }
+}
